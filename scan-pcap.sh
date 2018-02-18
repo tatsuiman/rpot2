dddfe8d67bf47616dbcf9d92d3551b5f8b507c7d
tatsu-i <t_ito@cyberdefense.jp>
2018-02-18 18:45:28 +0900

    update scan script

--- scan-pcap.sh
+++ scan-pcap.sh
@@ -3,5 +3,5 @@ cp ./config/${2}-hunter.bro ./config/output.bro
 sed -i -e "s/sensor-001/${3}/g" ./config/output.bro
 /usr/local/bro/bin/bro -r ${1} ./config/output.bro
 sudo suricata -c /usr/local/etc/suricata/suricata.yaml -r $1
-cd ./hunting
-./run.sh
+#cd ./hunting
+#./run.sh
